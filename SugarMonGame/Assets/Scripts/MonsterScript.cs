e50840c76c06841e655a7a8d001fbfecb955b4a4
Mark <markbotaish@gmail.com>
2019-06-07 14:46:00 -0400

    Init commit

---
+++ SugarMonGame/Assets/Scripts/MonsterScript.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterScript : MonoBehaviour
+{
+
+    private Vector3 _cameraPosition;
+    private Vector3 _forwardDirection;
+    private float _radius;
+    private Rigidbody _rigid;
+    private bool _hasBeenInitted = false;
+    private int _MoveCounter = 0;
+    private int _ChargeAtMove;
+    private SpawnMonsters sm;
+
+    public void Start()
+    {
+        _rigid = GetComponent<Rigidbody>();
+        sm = SpawnMonsters.instance;
+        _ChargeAtMove = Random.Range(2,7);
+    }
+
+    public void InitMonster(Vector3 pos, float radius)
+    {
+        _cameraPosition = pos;
+        Vector3 temp = new Vector3(transform.position.x, pos.y, transform.position.z);
+        _forwardDirection = (temp - pos).normalized;
+        _radius = radius;
+        _hasBeenInitted = true; 
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (_hasBeenInitted)
+        {
+            if(_MoveCounter < _ChargeAtMove)
+                checkPosition();
+            else
+            {
+                _rigid.velocity = (_cameraPosition - transform.position).normalized * 3.0f;
+            }
+        }
+       
+    }
+
+    void checkPosition()
+    {
+        if((transform.position - _cameraPosition).sqrMagnitude >= (_radius * _radius) || Vector3.Dot(_forwardDirection, transform.position - _cameraPosition) < 0 )//|| !TestPoint(gameObject.transform.position))
+        {
+            if(sm.GetNumOfMonstersAlive() > 1)
+            {
+                Vector3 vel = (sm.GetNewPosition(gameObject) - transform.position).normalized * Random.Range(2,10);
+                _rigid.velocity = vel;
+            }
+            else
+            {
+                _rigid.velocity = -_rigid.velocity;
+            }
+            _MoveCounter++;
+        }
+    }
+
+    bool TestPoint(Vector3 pos)
+    {
+        float mag = equation(new Vector2(pos.x, pos.z).magnitude);
+
+        if (pos.y < mag && pos.y > -mag)
+            return true;
+
+        return false;
+    }
+
+    //y = x
+    float equation(float x)
+    {
+        return x;
+    }
+
+}
---
+++ SugarMonGame/Assets/Scripts/SpawnMonsters.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnMonsters : MonoBehaviour
+{
+    public static SpawnMonsters instance;
+
+    public GameObject _monsterPrefab;
+    public float _radius = 10;
+
+    private Transform _cameraTransform;
+    private int counter = 0;
+    private List<GameObject> monsters = new List<GameObject>();
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _cameraTransform = GameObject.Find("Main Camera").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(++counter < 10)
+        {
+            GameObject monster = Instantiate(_monsterPrefab, transform.position, Quaternion.identity);
+            Vector3 vel = Random.onUnitSphere * Random.Range(2, 10);
+            monster.GetComponent<Rigidbody>().velocity = vel;
+            monster.GetComponent<MonsterScript>().InitMonster(_cameraTransform.position, _radius);
+            monsters.Add(monster);
+        }
+    }
+
+    public Vector3 GetNewPosition(GameObject obj)
+    {
+        GameObject newObj = null;
+
+        do
+        {
+            newObj = monsters[Random.Range(0, monsters.Count)];
+        } while (obj == newObj);
+
+
+        return newObj.transform.position;
+    }
+
+    public void RemoveMonster(GameObject obj){monsters.Remove(obj);}
+    public int GetNumOfMonstersAlive(){return monsters.Count;}
+}
