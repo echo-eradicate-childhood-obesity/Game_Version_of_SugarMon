f4fccc5972d8d5222c7991c111739a535be4a12a
Mark <markbotaish@gmail.com>
2019-07-18 15:56:30 -0400

    - reworked Erins random monster to be more efficient

--- SugarMonGame/Assets/Scripts/Shooting/SpawnMonsters.cs
+++ SugarMonGame/Assets/Scripts/Shooting/SpawnMonsters.cs
@@ -3,107 +3,62 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class SpawnMonsters : MonoBehaviour
 {
-        #region PUBLIC_VARS
-        public static SpawnMonsters instance;
+    #region PUBLIC_VARS
+    public static SpawnMonsters instance;
 
-        [Tooltip("The minions prefab of a monster")] public GameObject _monsterPrefab;
-        [Tooltip("The minions prefab of a monster v 2")] public GameObject _monsterCane;
-        [Tooltip("The minions prefab of a monster v 3")] public GameObject _monsterDonut;
-        [Tooltip("The minions prefab of a monster v 4")] public GameObject _monsterCan;
-        [Tooltip("The minions prefab of a monster v 5")] public GameObject _monsterConcentrate;
-       [Tooltip("The minions prefab of a monster v 6")] public GameObject _monsterGlucose;
-        [Tooltip("The max distance from the camera a monster can get")] public float _radius = 10;
-        #endregion
+    [Tooltip("The minions prefab of a monster")]                    public List<GameObject> _monsterPrefabs;
+    [Tooltip("The max distance from the camera a monster can get")] public float _radius = 10;
+    [Tooltip("Win Panel reference")]                                public GameObject _winPanel;
+    #endregion
 
-        #region PRIVATE_VARS
-        private Transform _cameraTransform;                              // The transfrom of the camera in the scene
-        private List<GameObject> _monsters = new List<GameObject>();     // The list of monsters
-        private bool _shouldSpawn = true;
-        #endregion
+    #region PRIVATE_VARS
+    private Transform _cameraTransform;                              // The transfrom of the camera in the scene
+    private List<GameObject> _monsters = new List<GameObject>();     // The list of monsters
+    private GameObject _canvas;
+    #endregion
 
-        private void Awake()
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _cameraTransform = GameObject.Find("ARCore Device").transform.GetChild(0);
+        _canvas = GameObject.Find("Canvas");
+        StartCoroutine(SpawnEnemies(10));
+    }
+
+    /// <summary>
+    /// This fucntion is used to spawn a number of monsters.
+    /// </summary>
+    IEnumerator SpawnEnemies(int num)
+    {
+        while (_monsters.Count < num)
         {
-            instance = this;
-        }
-
-        // Start is called before the first frame update
-        void Start()
-        {
-            _cameraTransform = GameObject.Find("ARCore Device").transform.GetChild(0);
-        }
-
-        // Update is called once per frame
-        void Update()
-        {
-            if (_shouldSpawn)
-                SpawnMonster();
-        }
-
-        /// <summary>
-        /// This fucntion is used to spawn 10 monsters. This function will get called when 
-        /// all have died or the game has just started.
-        /// </summary>
-        void SpawnMonster()
-        {
-            if (_monsters.Count < 10)
+            GameObject monster = Instantiate(_monsterPrefabs[Random.Range(0, _monsterPrefabs.Count)], transform.position, Quaternion.identity);
+            Vector3 vel = Random.onUnitSphere * Random.Range(2, 5);
+            monster.GetComponent<Rigidbody>().velocity = vel;
+            PlayerInfoScript player = PlayerInfoScript.instance;
+            //If the PlayerInfoScript exists (started game from menu)
+            if (player)
             {
-                System.Random rnd = new System.Random();
-                int monType = rnd.Next(1, 7);
-                if (monType == 1)
-                {
-                    GameObject monster = Instantiate(_monsterPrefab, transform.position, Quaternion.identity);
-                    Vector3 vel = Random.onUnitSphere * Random.Range(2, 10);
-                    monster.GetComponent<Rigidbody>().velocity = vel;
-                    monster.GetComponent<ChargingMonsterScript>().InitMonster(_cameraTransform.position, _radius);
-                    _monsters.Add(monster);
-                }
-                else if (monType == 2)
-                {
-                    GameObject monster = Instantiate(_monsterCane, transform.position, Quaternion.identity);
-                    Vector3 vel = Random.onUnitSphere * Random.Range(2, 10);
-                    monster.GetComponent<Rigidbody>().velocity = vel;
-                    monster.GetComponent<ChargingMonsterScript>().InitMonster(_cameraTransform.position, _radius);
-                    _monsters.Add(monster);
-                }
-                else if (monType == 3)
-                {
-                    GameObject monster = Instantiate(_monsterDonut, transform.position, Quaternion.identity);
-                    Vector3 vel = Random.onUnitSphere * Random.Range(2, 10);
-                    monster.GetComponent<Rigidbody>().velocity = vel;
-                    monster.GetComponent<ChargingMonsterScript>().InitMonster(_cameraTransform.position, _radius);
-                    _monsters.Add(monster);
-                }
-                else if (monType == 4)
-                {
-                    GameObject monster = Instantiate(_monsterCan, transform.position, Quaternion.identity);
-                    Vector3 vel = Random.onUnitSphere * Random.Range(2, 10);
-                    monster.GetComponent<Rigidbody>().velocity = vel;
-                    monster.GetComponent<ChargingMonsterScript>().InitMonster(_cameraTransform.position, _radius);
-                    _monsters.Add(monster);
-                }
-                else if (monType == 5)
-                {
-                    GameObject monster = Instantiate(_monsterGlucose, transform.position, Quaternion.identity);
-                    Vector3 vel = Random.onUnitSphere * Random.Range(2, 10);
-                    monster.GetComponent<Rigidbody>().velocity = vel;
-                    monster.GetComponent<ChargingMonsterScript>().InitMonster(_cameraTransform.position, _radius);
-                    _monsters.Add(monster);
-                }
-                else
-                {
-                    GameObject monster = Instantiate(_monsterConcentrate, transform.position, Quaternion.identity);
-                    Vector3 vel = Random.onUnitSphere * Random.Range(2, 10);
-                    monster.GetComponent<Rigidbody>().velocity = vel;
-                    monster.GetComponent<ChargingMonsterScript>().InitMonster(_cameraTransform.position, _radius);
-                    _monsters.Add(monster);
-                }
+                //Each level is offset but 10 levels to make it more challenging in other groups
+                int level = player.GetCurrentGroup() * 25 + player.GetLevelInSugarGroup(player.GetCurrentGroup());
+                print("Level " + level);
+                monster.GetComponent<MonsterScript>().InitMonster(_cameraTransform.position, _radius, _canvas, level);
             }
-            else
-        {
-            _shouldSpawn = false;
+            else //For testing purposes 
+                monster.GetComponent<MonsterScript>().InitMonster(_cameraTransform.position, _radius, _canvas, 0);
+
+            monster.transform.LookAt(_cameraTransform.position);
+            _monsters.Add(monster);
+            yield return null;
         }
     }
 
@@ -115,13 +70,16 @@ public class SpawnMonsters : MonoBehaviour
     public Vector3 GetNewPosition(GameObject obj)
     {
         GameObject newObj = null;
-
-        do
+        if (_monsters.Count > 1)
         {
-            newObj = _monsters[Random.Range(0, _monsters.Count)];
+            do
+            {
+                newObj = _monsters[Random.Range(0, _monsters.Count)];
+            } while (obj == newObj);
+        }
+        else
+            newObj = gameObject;
 
-        } while (obj == newObj);
-        
         return newObj.transform.position;
     }
 
@@ -129,10 +87,22 @@ public class SpawnMonsters : MonoBehaviour
     /// This function is used to remove a destroyed monster from the list.
     /// </summary>
     /// -This function gets called from the MonterScript-
-    public void RemoveMonster(GameObject obj){
+    public void RemoveMonster(GameObject obj)
+    {
         _monsters.Remove(obj);
-        if (_monsters.Count <= 0)
-            _shouldSpawn = true;
+        if (_monsters.Count <= 0 && !PlayerScript.instance.IsDead())
+        {
+            PlayerInfoScript info = PlayerInfoScript.instance;
+            if (PlayerScript.instance._IsTesting) //If you are using a computer, lock the mouse to the middle of the Game window
+                Cursor.lockState = CursorLockMode.None;
+
+            //Update the stats of the player
+            info.AddLevelInSugarGroup();
+            _winPanel.SetActive(true); //Display win screen 
+            string stats = "Coins: " + info.GetCoinsFromLevel() + "\nXP: " + info.GetXpFromLevel();
+            _winPanel.transform.Find("Stats").GetComponent<TextMeshProUGUI>().text = stats; //Update stats
+        }
+
     }
 
     /// <summary>
